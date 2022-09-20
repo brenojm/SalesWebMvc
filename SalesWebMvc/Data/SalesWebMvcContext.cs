c50c2795aac990bdc3a9abf74d07c4f10414a41c
Breno Jardim Magrani <breno.jm25@gmail.com>
2022-09-20 16:41:38 -0300

    First Migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using SalesWebMvc.Models;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Data
 {
