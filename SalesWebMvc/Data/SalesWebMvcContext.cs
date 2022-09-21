2c2a32add9761eef2d6ba697c68719b919674fa1
Breno Jardim Magrani <breno.jm25@gmail.com>
2022-09-21 10:39:28 -0300

    Other Entities and second migration

--- SalesWebMvc/Data/SalesWebMvcContext.cs
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -10,5 +10,7 @@ namespace SalesWebMvc.Data
         }
 
         public DbSet<SalesWebMvc.Models.Department> Department { get; set; }
+        public DbSet<SalesWebMvc.Models.Seller> Seller { get; set; }
+        public DbSet<SalesWebMvc.Models.SalesRecord> SalesRecord { get; set; }
     }
 }
--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMvc.Models
 {
@@ -9,5 +8,26 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
