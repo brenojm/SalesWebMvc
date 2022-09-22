fe8fbc4cc2340fe2c75a48bd359f0ecbaec4c4a7
Breno Jardim Magrani <breno.jm25@gmail.com>
2022-09-21 22:30:04 -0300

    Implementing group search

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -41,7 +41,7 @@ namespace SalesWebMvc.Services
                 _context.Seller.Remove(obj);
                 await _context.SaveChangesAsync();
             }
-            catch(DbUpdateException e)
+            catch(DbUpdateException)
             {
                 throw new IntegrityException("Can't delete the seller because he/she has sales");
             }
